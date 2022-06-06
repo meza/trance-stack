fe01b188763be5931a20653d869413c028382131
meza <meza@meza.hu>
2022-06-06 09:41:04 +0100

    chore: simplifying

--- dev.js
+++ dev.js
@@ -1,7 +1,6 @@
 process.env.PORT = process.env.PORT ?? 3000;
 process.env.ARC_LOCAL = process.env.ARC_LOCAL ?? 1;
 process.env.ARC_TABLES_PORT = process.env.ARC_TABLES_PORT ?? 5555;
-process.env.STATIC_PATH = '_static';
 
 const arc = require('@architect/architect');
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,5 @@
-import { useTranslation } from 'react-i18next';
 export default function Index() {
-  const { t } = useTranslation();
   return (
-    <div>{t('greeting')} World</div>
+    <div>Hello World</div>
   );
 }

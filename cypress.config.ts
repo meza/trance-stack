81951ececb9887901fbb5146c4036c3931e67836
meza <meza@meza.hu>
2022-06-14 12:49:11 +0200

    chore: upgraded to align with the upstream

---
+++ cypress.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents: (on, config) => {
+      const port = process.env.PORT ?? '3000';
+      const configOverrides: Partial<Cypress.PluginConfigOptions> = {
+        baseUrl: `http://localhost:${port}`,
+        video: !process.env.CI,
+        screenshotOnRunFailure: !process.env.CI
+      };
+
+      // To use this:
+      // cy.task('log', whateverYouWantInTheTerminal)
+      on('task', {
+        log: (message) => {
+          console.log(message);
+
+          return null;
+        }
+      });
+
+      return { ...config, ...configOverrides };
+    }
+  }
+});

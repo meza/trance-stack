c9935d5c04c904860bc6d3271a24cb834bbd2032
meza <meza@meza.hu>
2023-02-13 18:30:55 +0000

    chore: adding hotjar

--- src/root.tsx
+++ src/root.tsx
@@ -1,12 +1,14 @@
-import type { MetaFunction } from '@remix-run/node';
+import type { LinksFunction, MetaFunction } from '@remix-run/node';
+
 import {
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
+  ScrollRestoration, useLoaderData
 } from '@remix-run/react';
+import { json } from '@remix-run/node';
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
@@ -14,19 +16,41 @@ export const meta: MetaFunction = () => ({
   viewport: 'width=device-width,initial-scale=1'
 });
 
-export default function App() {
+export const loader = async () => {
+  return json({ hotjarId: process.env.HOTJAR_ID || 3365028 });
+};
+
+const App = () => {
+  const { hotjarId } = useLoaderData<typeof loader>();
   return (
     <html lang="en">
       <head>
-        <Meta />
-        <Links />
+        <Meta/>
+        <Links/>
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        <Outlet/>
+        <ScrollRestoration/>
+        <script
+          async
+          id="hotjar-tracker"
+          dangerouslySetInnerHTML={{
+            __html: `(function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:${hotjarId},hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `
+          }}
+        />
+        <Scripts/>
+        <LiveReload/>
       </body>
     </html>
   );
-}
+};
+
+export default App;

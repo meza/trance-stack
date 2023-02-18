aca6bac31745d80ef0ca4008057adc71fba57f64
meza <meza@meza.hu>
2023-02-18 20:42:08 +0000

    ci: debugging

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:3333',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry'
@@ -86,7 +86,7 @@ export default defineConfig({
   webServer: {
     command: 'pnpm exec dev',
     url: 'http://localhost:3333/',
-    timeout: 30 * 1000,
+    timeout: 5 * 60 * 1000,
     reuseExistingServer: false
   }
 });
--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3333/');
+  await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/REPL_APP_NAME/);
 });
 
 test('has hello world', async ({ page }) => {
-  await page.goto('http://localhost:3333/');
+  await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page.locator('h1')).toHaveText(/Hello World/);

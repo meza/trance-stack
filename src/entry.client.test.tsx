eeb483d48f8a4ece4a5ca12d96e52aa11b5faa56
meza <meza@meza.hu>
2023-02-18 00:53:44 +0000

    chore(vitest): checkpoint

--- src/entry.client.test.tsx
+++ src/entry.client.test.tsx
@@ -1,16 +1,60 @@
-import { cleanup } from '@testing-library/react';
-import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { startTransition } from 'react';
+import { hydrateRoot } from 'react-dom/client';
+import { beforeEach, describe, expect, it, vi, afterEach } from 'vitest';
+import { initClientI18n } from '~/i18n';
 
-describe('entry.client', () => {
+vi.mock('react');
+vi.mock('react-i18next');
+vi.mock('~/i18n');
+vi.mock('react-dom/client');
+vi.mock('i18next', () => ({
+  default: 'i18next'
+}));
+vi.mock('~/components/Mixpanel');
+vi.mock('@remix-run/react');
+
+describe('The Client Entrypoint', () => {
   beforeEach(() => {
     vi.resetAllMocks();
+    vi.useFakeTimers();
+    vi.mocked(initClientI18n).mockResolvedValue(undefined as never);
+    vi.mocked(startTransition).mockImplementation((callback: CallableFunction) => {
+      callback();
+    });
+    vi.resetModules();
   });
 
   afterEach(() => {
-    cleanup();
+    // restoring date after each test run
+    vi.useRealTimers();
   });
 
-  it('should work', () => {
-    expect(1).toBe(1);
+  it('Should work on Safari', async () => {
+    await import('./entry.client');
+    await vi.advanceTimersByTimeAsync(1000);
+
+    expect(vi.mocked(hydrateRoot)).toHaveBeenCalledOnce();
+  });
+
+  it('Should work on Other Browsers', async () => {
+    vi.useRealTimers();
+    vi.stubGlobal('requestIdleCallback', (callback: CallableFunction) => {
+      callback();
+    });
+    await import('./entry.client');
+
+    expect(vi.mocked(hydrateRoot)).toHaveBeenCalledOnce();
+    const hydrateRootCalls = vi.mocked(hydrateRoot).mock.calls[0];
+    expect(hydrateRootCalls[0]).toEqual(document);
+    expect(hydrateRootCalls[1]).toMatchInlineSnapshot(`
+      <I18nextProvider
+        i18n="i18next"
+      >
+        <Mixpanel />
+        <UNDEFINED>
+          <RemixBrowser />
+        </UNDEFINED>
+      </I18nextProvider>
+    `);
   });
 });

78f3a8e3d89b6de330a2e097c6a41a031dadd0b1
meza <meza@meza.hu>
2023-02-18 19:47:53 +0000

    ci: added playwright

---
+++ tests/e2e/example.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('http://localhost:3333/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/REPL_APP_NAME/);
+});
+
+test('has hello world', async ({ page }) => {
+  await page.goto('http://localhost:3333/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page.locator('h1')).toHaveText(/Hello World/);
+});

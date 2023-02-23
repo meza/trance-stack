90de768319d1bb415e593688fbb14d28bf5a5e03
meza <meza@meza.hu>
2023-02-23 22:47:03 +0000

    chore: fix deploy script

--- deployment/cdk.ts
+++ deployment/cdk.ts
@@ -8,8 +8,10 @@ const app = new cdk.App();
 const envName = app.node.tryGetContext('environmentName');
 
 // eslint-disable-next-line no-new
-new CdkStack(app, `${name}-${envName}-deployment`, {
+new CdkStack(app, `${name}-deployment`, {
   description: `CDK Stack for ${name}, version: ${version}`,
+  stackName: `${name}-${envName}`,
+  analyticsReporting: true,
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: 'us-east-1' // must be us-east-1 to allow Lambda@Edge

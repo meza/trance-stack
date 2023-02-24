a149bce2eaaf227bfe8a3d6057391d5dfa44a959
meza <meza@meza.hu>
2023-02-24 22:21:09 +0000

    fix: extracted the cache to its own file

--- deployment/stacks/EphemeralStack.ts
+++ deployment/stacks/EphemeralStack.ts
@@ -1,27 +1,9 @@
 import path from 'node:path';
-import { HttpApi } from '@aws-cdk/aws-apigatewayv2-alpha';
-import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
-import { CfnOutput, Duration, Stack, Tags } from 'aws-cdk-lib';
-import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
-import {
-  AllowedMethods, CacheCookieBehavior, CacheHeaderBehavior,
-  CachePolicy, CacheQueryStringBehavior,
-  Distribution,
-  OriginRequestCookieBehavior,
-  OriginRequestHeaderBehavior,
-  OriginRequestPolicy,
-  OriginRequestQueryStringBehavior,
-  PriceClass,
-  ViewerProtocolPolicy
-} from 'aws-cdk-lib/aws-cloudfront';
-import { HttpOrigin, S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { CfnOutput, Stack, Tags } from 'aws-cdk-lib';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
-import { ARecord, HostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
-import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
 import { RemixApiGateway } from '../lib/Constructs/RemixApiGateway';
 import { RemixDeployment } from '../lib/Constructs/RemixDeployment';
 import type { StackProps } from 'aws-cdk-lib';
-import type { AddBehaviorOptions } from 'aws-cdk-lib/aws-cloudfront';
 import type { Construct } from 'constructs';
 
 export class EphemeralStack extends Stack {
--- deployment/stacks/ProductionStack.ts
+++ deployment/stacks/ProductionStack.ts
@@ -1,103 +1,55 @@
 import path from 'node:path';
-import { Duration, Stack } from 'aws-cdk-lib';
+import { CfnOutput, Stack, Tags } from 'aws-cdk-lib';
 import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
-import {
-  AllowedMethods, CacheCookieBehavior, CacheHeaderBehavior,
-  CachePolicy, CacheQueryStringBehavior,
-  Distribution,
-  OriginRequestCookieBehavior,
-  OriginRequestHeaderBehavior,
-  OriginRequestPolicy,
-  OriginRequestQueryStringBehavior,
-  PriceClass,
-  ViewerProtocolPolicy
-} from 'aws-cdk-lib/aws-cloudfront';
-import { HttpOrigin, S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { ARecord, HostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
 import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
 import { RemixApiGateway } from '../lib/Constructs/RemixApiGateway';
+import { RemixCDN } from '../lib/Constructs/RemixCDN';
 import { RemixDeployment } from '../lib/Constructs/RemixDeployment';
 import type { StackProps } from 'aws-cdk-lib';
-import type { AddBehaviorOptions } from 'aws-cdk-lib/aws-cloudfront';
 import type { Construct } from 'constructs';
 
 export class ProductionStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const environmentName = scope.node.tryGetContext('environmentName').replace(/[^a-zA-Z0-9-]/g, '-');
     const hostedZoneName = scope.node.tryGetContext('hostedZoneName');
     const domainName = scope.node.tryGetContext('domainName');
     const certificateArn = scope.node.tryGetContext('certificateArn');
-
     const certificate = Certificate.fromCertificateArn(this, 'Certificate', certificateArn);
 
-    const formatName = (name: string) => `${id}-${environmentName}-${name}`;
-
-    const remixDeployment = new RemixDeployment(this, formatName('RemixDeployment'), {
+    const remixDeployment = new RemixDeployment(this, 'RemixDeployment', {
       publicDir: path.join(__dirname, '../../public'),
       server: path.join(__dirname, '../../server/index.js'),
       runtime: Runtime.NODEJS_18_X
     });
 
-    const api = new RemixApiGateway(this, formatName('RemixApiGateway'), remixDeployment);
+    const api = new RemixApiGateway(this, 'RemixApiGateway', remixDeployment);
 
-    const originRequestPolicy = new OriginRequestPolicy(this, formatName('RequestHandlerPolicy'), {
-      originRequestPolicyName: formatName('RequestHandlerPolicy'),
-      queryStringBehavior: OriginRequestQueryStringBehavior.all(),
-      cookieBehavior: OriginRequestCookieBehavior.all(),
-      // https://stackoverflow.com/questions/65243953/pass-query-params-from-cloudfront-to-api-gateway
-      headerBehavior: OriginRequestHeaderBehavior.allowList(
-        'Accept-Charset', 'Origin', 'Access-Control-Request-Method', 'Access-Control-Request-Headers', 'Referer', 'Accept-Language', 'Accept-Datetime'
-      )
-    });
-
-    const cachePolicy = new CachePolicy(this, formatName('CachePolicy'), {
-      cachePolicyName: formatName('CachePolicy'),
-      defaultTtl: Duration.seconds(86400),
-      minTtl: Duration.seconds(0),
-      maxTtl: Duration.seconds(31536000),
-      cookieBehavior: CacheCookieBehavior.none(),
-      headerBehavior: CacheHeaderBehavior.allowList('Origin', 'Access-Control-Request-Method', 'Access-Control-Request-Headers', 'Referer', 'Accept-Language', 'Accept-Datetime'),
-      queryStringBehavior: CacheQueryStringBehavior.none()
-    });
-    const requestHandlerBehavior: AddBehaviorOptions = {
-      allowedMethods: AllowedMethods.ALLOW_ALL,
-      viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-      cachePolicy: cachePolicy,
-      originRequestPolicy: originRequestPolicy
-    };
-
-    const assetOrigin = new S3Origin(remixDeployment.staticBucket().bucket);
-    const assetBehaviorOptions = {
-      viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS
-    };
-
-    const distribution = new Distribution(this, 'HUHDistribution', {
-      domainNames: [domainName],
+    const cdn = new RemixCDN(this, 'RemixCDN', {
+      remixDeployment: remixDeployment,
+      api: api,
       certificate: certificate,
-      defaultBehavior: {
-        compress: true,
-        origin: new HttpOrigin(api.url()),
-        ...requestHandlerBehavior
-      },
-      priceClass: PriceClass.PRICE_CLASS_ALL,
-      additionalBehaviors: {
-        '/_static/*': {
-          origin: assetOrigin,
-          ...assetBehaviorOptions
-        }
-      }
+      domainNames: [domainName]
     });
 
     // eslint-disable-next-line no-new
-    new ARecord(this, formatName('AliasRecord'), {
-      zone: HostedZone.fromLookup(this, formatName('HostedZone'), {
+    new ARecord(this, 'AliasRecord', {
+      zone: HostedZone.fromLookup(this, 'HostedZone', {
         domainName: hostedZoneName
       }),
       recordName: domainName,
-      target: RecordTarget.fromAlias(new CloudFrontTarget(distribution))
+      target: RecordTarget.fromAlias(new CloudFrontTarget(cdn.distribution()))
     });
+
+    // eslint-disable-next-line no-new
+    new CfnOutput(this, 'domain', {
+      description: 'The URL of the app',
+      value: domainName
+    });
+
+    Tags.of(this).add('ephemeral', 'false');
+    Tags.of(this).add('production', 'true');
   }
 }

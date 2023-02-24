ccb0cf52d0b22e06f191084db023ca646d6e994b
meza <meza@meza.hu>
2023-02-24 17:17:14 +0000

    chore: debugging

--- deployment/githubEnv.ts
+++ deployment/githubEnv.ts
@@ -1,4 +1,5 @@
 import fs from 'node:fs';
+console.log(process.env);
 if (!process.env.GITHUB_STEP_SUMMARY) {
   process.env.GITHUB_STEP_SUMMARY = 'deploymentSummary.md';
   // eslint-disable-next-line no-sync

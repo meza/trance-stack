6e5eacb03f0063c3e596e06f031c655cb25f6dda
meza <meza@meza.hu>
2023-02-24 19:10:03 +0000

    chore: fiddling with gh actions

--- deployment/githubActionSupport.ts
+++ deployment/githubActionSupport.ts
@@ -2,6 +2,7 @@ import './githubEnv'; // for local testing purposes
 import fs from 'node:fs';
 import path from 'node:path';
 import * as core from '@actions/core';
+import github from '@actions/github';
 
 const file = path.resolve(process.argv[2]);
 core.info(`Looking for deployment result file at ${file}`);
@@ -19,16 +20,38 @@ if (fs.existsSync(file)) {
     const stack = Object.keys(outputs)[0];
     const apiName = `https://${typedOutputs[stack].ApiUrl}`;
 
-    core.summary
+    const summary = core.summary
       .addHeading('Deployment details')
       .addBreak()
       .addRaw(`✅ Your stack: <code>${stack}</code> has been successfully deployed.`)
       .addSeparator()
       .addRaw('You can access your API at the following URL: ')
-      .addLink(apiName, apiName)
-      .write().then(() => {
+      .addLink(apiName, apiName);
+
+    const text = summary.toString();
+
+    summary.write().then(() => {
+      core.endGroup();
+    });
+
+    if (github.context.eventName === 'pull_request') {
+      core.startGroup('Deployment summary for a PR');
+      const context = github.context;
+      const token = process.env.GITHUB_TOKEN || core.getInput('token', { required: true });
+      const octokit = github.getOctokit(token);
+      const repository = context.repo.repo;
+      const owner = context.repo.owner;
+      const issueNumber = process.env.ISSUE_NUMBER;
+      octokit.rest.issues.createComment({
+        owner: owner,
+        repo: repository,
+        // eslint-disable-next-line camelcase
+        issue_number: issueNumber,
+        body: text
+      } as never).then(() => {
         core.endGroup();
       });
+    }
   });
 } else {
   core.warning('No deployment result file found');

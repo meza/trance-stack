e38b2a8591f3275b1618a12e364bb480e2088e93
meza <meza@meza.hu>
2023-02-28 17:55:34 +0000

    chore: fixed pnpm

--- playwright.config.ts
+++ playwright.config.ts
@@ -86,7 +86,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm dev',
+    command: 'npm run dev',
     url: 'http://localhost:3000',
     timeout: 1 * 60 * 1000,
     reuseExistingServer: !process.env.CI

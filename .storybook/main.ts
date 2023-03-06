5e79d3e6be8c34387219d8c62f15b0e11579e656
meza <meza@meza.hu>
2023-03-06 16:59:53 +0000

    docs: replaced the faulty storybook i18next plugin

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,7 @@ const storybookConfig: StorybookConfig = {
   addons: [
     '@storybook/addon-a11y',
     '@storybook/addon-links',
-    'storybook-react-i18next',
+    '@meza/storybook-react-i18next',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     {

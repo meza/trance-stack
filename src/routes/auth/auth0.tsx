2028518b1ef3326607470062bfad33504cd9d445
meza <meza@users.noreply.github.com>
2023-03-17 12:37:32 +0000

    refactor: updated the auth code to work with v2 of the auth0 package

--- src/routes/auth/auth0.tsx
+++ src/routes/auth/auth0.tsx
@@ -2,6 +2,5 @@ import { authenticator } from '~/auth.server';
 import type { ActionFunction } from '@remix-run/node';
 
 export const action: ActionFunction = () => {
-  const forceLogin = false;
-  authenticator.authorize(forceLogin);
+  authenticator.authorize();
 };
